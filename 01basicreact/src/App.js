e38bda3bb6fc5b12f734ce3a54a9a69bfe87a440
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-02-16 14:35:21 +0530

    Updated project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+            <h1>hello word | Name Abhishek vishwakarma 
+              <h3>Day one of my learning:-</h3>
+              <p></p>
+
+            </h1>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+      <h1>hello react with vite | Abhishek Vishwakarma</h1>
+  )
+}
+
+export default App
