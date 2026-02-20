122afeb5eb93a9f1f3d159d36377a0933fd2f250
Abhishek Kumar <166474204+Abhishekkumar0091@users.noreply.github.com>
2026-02-20 16:11:57 +0530

    initial commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,8 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp()
+{
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+createRoot(document.getElementById('root')).
+render(
+    <MyApp />
 )
